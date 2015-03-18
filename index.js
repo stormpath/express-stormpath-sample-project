0054d5040c099e67450426a0a5e667d35b24eeaf
Randall Degges <r@rdegges.com>
2015-03-18 12:07:06 -0700

    Adding some settings.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var bodyParser = require('body-parser');
 var express = require('express');
 var stormpath = require('express-stormpath');
+
+var routes = require('./lib/routes');
 
 /**
  * Globals.
@@ -9,13 +12,27 @@ var stormpath = require('express-stormpath');
 var app = express();
 
 /**
+ * Application settings.
+ */
+app.set('view engine', 'jade');
+app.set('views', './lib/views');
+
+/**
  * Middleware initialization.
  */
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(stormpath.init(app, {
   apiKeyId: process.env.STORMPATH_API_KEY_ID,
   apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
-  application: process.env.STORMPATH_APPLICATION_HREF
+  application: process.env.STORMPATH_APPLICATION_HREF,
+  redirectUrl: '/profile',
+  expandCustomData: true
 }));
+
+/**
+ * Route initialization.
+ */
+app.use('/', routes);
 
 /**
  * Start the web server.
