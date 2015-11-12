236f316f227a56ade7ad71c73c42a29bd6eb55fd
Robert <robert@stormpath.com>
2015-11-11 19:02:16 -0800

    copy of config

--- server.js
+++ server.js
@@ -24,9 +24,46 @@ app.locals.siteName = 'Express-Stormpath Example Project';
 
 app.use(stormpath.init(app, {
   website: true,
+  //api: true,
+  application: {
+    href: process.env.STORMPATH_APPLICATION_HREF
+  },
+  secretKey: process.env.MY_SECRET,
+
   expand: {
-    customData: true
-  }
+    // Custom data is not coming in expanded even though this is set.
+    customData: true,
+  },
+  web: {
+    verifyEmail: {
+      enabled: true
+    },
+    me: {
+      enabled: true,
+      uri: '/api/v1/me'
+    },
+    login: {
+      uri: '/api/v1/auth/login',
+    },
+    register: {
+      uri: '/api/v1/auth/registration',  // Use a different URL
+      nextUri: '/',    // Where to send the user to, if auto login is enabled
+      fieldOrder: ['givenName', 'surname', 'email', 'password', 'passwordConfirm'],
+    },
+    forgotPassword: {
+      uri: '/api/v1/auth/forgot-password',
+      nextUri: '/',
+    },
+  },
+  postRegistrationHandler: function (account, req, res, next) {
+    //console.log('server/main postRegistrationHandler account', account);
+    next();
+  },
+  postLoginHandler: function (account, req, res, next) {
+    //console.log('server/main postLoginHandler account', account);
+    res.send(account).end();
+    //next();
+  },
 }));
 
 /**
